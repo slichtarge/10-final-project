2dd31a2c8fb3dfa9cfab836d42f1987f66ed18f4
slichtarge <slichtarge@wustl.edu>
2022-12-11 20:57:31 -0600

    initialized

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>ToDo App</h1>
+        <h1>Sara's Final WebDev Project</h1>
       </div>
     );
   }
